18749467c65e95e841b0c85d39e6880b6bed4dc9
Victoria Tejeda <vicyatm@gmail.com>
2021-11-15 14:03:28 -0600

    cambios en login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { useState } from "react";
 import { FormLogin } from "./FormLogin";
-import { singIn } from "../firebaseconfig";
 import { useNavigate } from "react-router";
 import logo from "../assets/logo.png";
 import "../Scss/Login.scss";
+import {signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseconfig";
 
 export const Login = () => {
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const handleLogin = async (email, password) => {
-    try {
-      singIn(email, password);
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log (user);
       navigate("order");
-    } catch (error) {
-      setError("Error en las credenciales");
-      setTimeout(() => setError(""), 3000);
-    }
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      setError(errorMessage)  
+      setTimeout(() => setError(''), 2000);
+      console.log (errorCode,errorMessage );
+      
+    });
   };
   return (
     <>
