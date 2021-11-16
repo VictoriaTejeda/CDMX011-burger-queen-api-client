2decfb28cb29f53a7b22505af229c404351d59f7
fanita90 <parroquini_90@hotmail.com>
2021-11-16 13:58:56 -0600

    se creó lcomponente not found

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import {
 } from "react-router-dom";
 import { Login } from './components/Login';
 import { Order } from './components/Order';
+import { PrivateRoute } from '../src/components/PrivateRoute'
+import { ErrorPage } from './components/ErrorPage';
 
 function App() {
-  const { isLoggedIn} = useState(false)
+  const { isLoggedIn } = useState(false)
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="order" element={<Order isLoggedIn={isLoggedIn} />} />
+        <Route path="order" element={<PrivateRoute> <Order isLoggedIn={isLoggedIn} /></PrivateRoute>} />
+        <Route path="*" element={< ErrorPage/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import { Route, Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { auth } from "../firebaseconfig";
 
 // eslint-disable-next-line react/prop-types
-export const PrivateRoute = ({ component: Component, ...rest }) => {
-    const { currentUser } = auth();
-   // console.log('user', currentUser)
-  
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          return currentUser ? <Component {...props} /> : <Navigate to='/' />
-        }}
-      >
-  
-      </Route>
-    )
-  }
+export const PrivateRoute = ({ children }) => {
+  const user = auth.currentUser;
+  return user ? children : <Navigate to="/" />;
+}
