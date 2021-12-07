56c6f28e64bd4cb08ac5e3d1398c05a2c4fa93a9
fanita90 <parroquini_90@hotmail.com>
2021-12-06 18:46:45 -0600

    Se modificco el status de la data en waiterPage

--- src/components/waiter/OrderDone.jsx
+++ src/components/waiter/OrderDone.jsx
@@ -2,28 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { auth } from "../../firebaseconfig";
 import { helpHttp } from "../../helpers/helpHttp";
-import logo from "../../assets/logo.png"
-import menu from "../../assets/menu.png"
-import "../../Scss/OrdenDone.scss"
+import logo from "../../assets/logo.png";
+import menu from "../../assets/menu.png";
+import "../../Scss/OrdenDone.scss";
 
 export const OrderDone = (isLoggedIn) => {
   const [data, setData] = useState([]);
   const navigate = useNavigate();
-  const user = auth.currentUser
-
-const waiterEmail=()=>{
-if (user) {
-    isLoggedIn
-    const email= user.email;
-   setTimeout(() => email, 1000);
- return email;
-  } else {
-    console.log('sin usuario')
-  }  
-}
-console.log(waiterEmail());
-
-  
+  const user = auth.currentUser;
+  const [db, setDb] = useState([]);
 
   let api = helpHttp();
   let url = "http://localhost:5000/orders";
@@ -38,22 +25,68 @@ console.log(waiterEmail());
     });
   }, []);
 
-   
-  const products = data.filter((p) => p.status  == "listo" && p.waiter == waiterEmail());
- 
+  const waiterEmail = () => {
+    if (user) {
+      isLoggedIn;
+      const email = user.email;
+      setTimeout(() => email, 1000);
+      return email;
+    } else {
+      console.log("sin usuario");
+    }
+  };
+  console.log(waiterEmail());
+
+  const products = data.filter(
+    (p) => p.status == "listo" && p.waiter == waiterEmail()
+  );
+
+  const updateOrderFinish = (products) => {
+    console.log("entre a updateOrderFinish");
+    let endpoint = `${url}/${products.id}`;
+
+    console.log(endpoint);
+
+    let options = {
+      body: { status: "entregado" },
+      headers: { "content-type": "application/json" },
+    };
+    console.log(options);
+
+    api.patch(endpoint, options).then((res) => {
+      console.log(res);
+      console.log(options);
+      if (!res.err) {
+        let newData = db.map((el) => (el.id === products.id ? products : el));
+        console.log("🚀:O ", newData);
+        setDb(newData);
+      } else {
+        console.log(res);
+        //setError(res)
+      }
+    });
+  };
+
+  function refreshPage() {
+    window.location.reload();
+  }
 
   return (
-
     <>
-   <div>
-      <img src={logo} alt="logo-img" className="logo-order" />
-      <div className= "icon-menu">
-          <img src={menu} alt="menu" className="return-menu" onClick ={() => {
+      <div>
+        <img src={logo} alt="logo-img" className="logo-order" />
+        <div className="icon-menu">
+          <img
+            src={menu}
+            alt="menu"
+            className="return-menu"
+            onClick={() => {
               navigate("/waiter");
-            }} />
-          </div>
-          <h3>hola {waiterEmail()}</h3>
-          <section className = "card-done">
+            }}
+          />
+        </div>
+        <h3>hola {waiterEmail()}</h3>
+        <section className="card-done">
           {products.map((op) => (
             <div key={op.id}>
               <div className="card-cocina">
@@ -71,7 +104,8 @@ console.log(waiterEmail());
                 <p className="status">{op.status}</p>
                 <button
                   onClick={() => {
-                   console.log("listo");
+                    updateOrderFinish(op);
+                    refreshPage();
                   }}
                 >
                   Terminar preparación
@@ -79,10 +113,8 @@ console.log(waiterEmail());
               </div>
             </div>
           ))}
-          </section>
+        </section>
       </div>
-    
     </>
-    
   );
 };
